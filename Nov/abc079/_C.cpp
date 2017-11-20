a4da28e8ef620ba359f6d74f61776da0dbebfc2f
seser1 <00ing.n@gmail.com>
2017-11-20 23:05:13 +0900

    abc079 _C(2)

--- Nov/abc079/_C.cpp
+++ Nov/abc079/_C.cpp
@@ -8,11 +8,12 @@
 using namespace std;
 
 vector<int> n;
-string ans;
+string ans = "";
 
 void df(int c, int sum, string s) {
+	if (ans != "") return;
 	if (c == n.size()) {
-		if(sum == 7)ans = s;
+		if (sum == 7)ans = s;
 		return;
 	}
 	df(c + 1, sum + n[c], s + "+" + std::to_string(n[c]));
@@ -21,14 +22,12 @@ void df(int c, int sum, string s) {
 }
 
 int main()
-{	
+{
 	string s;
 	cin >> s;
 
-	for (int i = 0; i < 4; i++) n.push_back(s[i] - '0');
-
+	for (int i = 0; i < s.size(); i++) n.push_back(s[i] - '0');
 	df(1, n[0], std::to_string(n[0]));
-
 	cout << ans << "=7" << endl;
 
 	return 0;

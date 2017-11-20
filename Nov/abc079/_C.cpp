3329a8d4e9a799df93ab43c5dbc149edf56f38ec
seseri <00ing.n@gmail.com>
2017-11-20 22:35:28 +0900

    abc079 _C

---
+++ Nov/abc079/_C.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include <string.h>
+#include <math.h>
+
+using namespace std;
+
+vector<int> n;
+string ans;
+
+void df(int c, int sum, string s) {
+	if (c == n.size()) {
+		if(sum == 7)ans = s;
+		return;
+	}
+	df(c + 1, sum + n[c], s + "+" + std::to_string(n[c]));
+	df(c + 1, sum - n[c], s + "-" + std::to_string(n[c]));
+	return;
+}
+
+int main()
+{	
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < 4; i++) n.push_back(s[i] - '0');
+
+	df(1, n[0], std::to_string(n[0]));
+
+	cout << ans << "=7" << endl;
+
+	return 0;
+}
